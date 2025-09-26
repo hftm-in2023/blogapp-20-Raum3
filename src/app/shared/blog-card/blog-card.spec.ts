a09c350185473eb689af24faf71bebcced3bce42
Daniel Wuester <daniel.wuester@hftm.ch>
2025-09-26 15:53:51 +0200

    test: diverse Test geschrieben und durchgeführt

--- src/app/shared/blog-card/blog-card.spec.ts
+++ src/app/shared/blog-card/blog-card.spec.ts
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, provideRouter } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { By } from '@angular/platform-browser';
 
 describe('BlogCard', () => {
   let component: BlogCard;
@@ -45,5 +46,72 @@ describe('BlogCard', () => {
 
     // assert
     expect(component).toBeTruthy();
+  });
+  // Test 1: Author
+  it('show correct author', () => {
+    fixture.componentRef.setInput('model', {
+      id: 1,
+      author: 'a author',
+      likedByMe: false,
+      title: 'A title',
+    } as Blog);
+
+    fixture.detectChanges();
+
+    const author = fixture.debugElement.query(
+      By.css('.blog-author p'),
+    ).nativeElement;
+    expect(author.innerText).toBe('a author');
+  });
+
+  // Test 2: Titel
+  it('show correct card-title', () => {
+    fixture.componentRef.setInput('model', {
+      id: 1,
+      author: 'a author',
+      likedByMe: false,
+      title: 'A title',
+    } as Blog);
+
+    fixture.detectChanges();
+
+    const title = fixture.debugElement.query(
+      By.css('.card-title'),
+    ).nativeElement;
+    expect(title.innerText).toBe('A title');
+  });
+
+  // Like-Button schwarz
+  it('show correct data', () => {
+    fixture.componentRef.setInput('model', {
+      id: 1,
+      author: 'a author',
+      likedByMe: false,
+      title: 'A title',
+    } as Blog);
+
+    fixture.detectChanges();
+
+    const likeButton = fixture.debugElement.query(
+      By.css('[data-testid="like-button-icon"]'),
+    );
+    expect(likeButton.styles['color']).toBe('black');
+  });
+
+  // Liked by rot
+  it('show correct data', () => {
+    fixture.componentRef.setInput('model', {
+      id: 1,
+      author: 'a author',
+      likedByMe: true,
+      title: 'A title',
+    } as Blog);
+
+    fixture.detectChanges();
+
+    const likeButton = fixture.debugElement.query(
+      By.css('[data-testid="like-button-icon"]'),
+    );
+    expect(likeButton.styles['color']).toBe('red');
   });
 });
